3122a7f3827fe28143284f59a2cd42f180c96ee8
Akshay Kadam <123861375+AK016@users.noreply.github.com>
2023-07-21 20:55:26 +0530

    Day 4 updated

--- RecipeVerse/src/main/java/com/masai/DTO/CustomerDTO.java
+++ RecipeVerse/src/main/java/com/masai/DTO/CustomerDTO.java
@@ -16,26 +16,68 @@ import jakarta.persistence.Table;
 @Table(name = "customers")
 public class CustomerDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+	@Column(name = "name", nullable = false)
+	private String name;
 
-    @Column(name = "email", nullable = false)
-    private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-    @ManyToMany
-    @JoinTable(
-            name = "customer_likes",
-            joinColumns = @JoinColumn(name = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "recipe_id")
-    )
-    private List<RecipeDTO> likedRecipes;
+	@ManyToMany
+	@JoinTable(name = "customer_likes", joinColumns = @JoinColumn(name = "customer_id"), inverseJoinColumns = @JoinColumn(name = "recipe_id"))
+	private List<RecipeDTO> likedRecipes;
 
-    // Constructors, getters, and setters
-    
-    
+	// Constructors, getters, and setters
+	public CustomerDTO() {
+		super();
+	}
+
+	public CustomerDTO(Long id, String name, String email, List<RecipeDTO> likedRecipes) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.likedRecipes = likedRecipes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<RecipeDTO> getLikedRecipes() {
+		return likedRecipes;
+	}
+
+	public void setLikedRecipes(List<RecipeDTO> likedRecipes) {
+		this.likedRecipes = likedRecipes;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerDTO id=" + id + ", name=" + name + ", email=" + email + ", likedRecipes=" + likedRecipes + "\n";
+	}
+
 }
-
